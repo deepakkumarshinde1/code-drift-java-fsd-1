834735f9ffa8ec5f21b91d2c94726fab2f638228
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2024-12-05 10:27:07 +0530

    Javascript Day-1

---
+++ Day-10 Javascript/main.js
@@ -0,0 +1,56 @@
+// popup
+
+let text = "deepak shinde";
+// text = "manish";
+text = 20;
+let checkType = typeof text;
+console.log(checkType);
+
+// + - / * %
+
+// += -= /= *= %=
+
+// > < === !== >= <=
+
+// && || !
+
+// (cond) ? true : false;
+
+if (2 === 3) {
+  // logic
+} else {
+  // false logic
+}
+
+if (2 === 2) {
+} else if (3 == 3) {
+} else if (4 === 4) {
+} else {
+}
+
+switch (10) {
+  case 1:
+    break;
+
+  case 2:
+    break;
+
+  default:
+    break;
+}
+
+let text1 = "deepak"; // string
+let text2 = 20; // number
+let text3 = "30"; // string
+text3 = Number(text3); // number
+let text4 = 2.5; // number
+let text5 = true; // boolean
+let text6 = null; // object
+let tex7; // undefined
+
+// string number boolean undefined object
+
+// object => null , object => array , object => JSObject
+
+// string number boolean and undefined , null => primitive types (variable by value)
+// object => array & js Object => non-primitive type (variable by reference)
